d3f88d70cbd2f18a577bbfae8c1a0770031796e6
Mihail <freon2006@bk.ru>
2018-10-09 03:47:49 +0500

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,34 +1,24 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-let beforePath;
-let afterPath;
-let diffPath;
-let result;
+const beforePath = './__tests__/__fixtures__/before.json';
+const afterPath = './__tests__/__fixtures__/after.json';
+const result1 = './__tests__/__fixtures__/result1.txt';
+const result2 = './__tests__/__fixtures__/result2.txt';
+
 
 const readFile = path => fs.readFileSync(path).toString();
 
 test('Test1 relative path', () => {
-  beforePath = './__tests__/__fixtures__/before.json';
-  afterPath = './__tests__/__fixtures__/after.json';
-  diffPath = './__tests__/__fixtures__/result1.txt';
-  result = readFile(diffPath);
-
-  expect(genDiff(beforePath, afterPath)).toBe(result);
+  expect(genDiff(beforePath, afterPath)).toBe(readFile(result1));
 });
 
 test('Test2 empty file', () => {
-  beforePath = './__tests__/__fixtures__/empty.json';
-  afterPath = './__tests__/__fixtures__/after.json';
-  diffPath = './__tests__/__fixtures__/result2.txt';
-  result = readFile(diffPath);
-
-  expect(genDiff(beforePath, afterPath)).toBe(result);
+  const emptyPath = './__tests__/__fixtures__/empty.json';
+  expect(genDiff(emptyPath, afterPath)).toBe(readFile(result2));
 });
 
 test('Test4 error path', () => {
-  beforePath = './__tests__/__fixtures__/unknown.json';
-  afterPath = './__tests__/__fixtures__/after.json';
-
-  expect(() => genDiff(beforePath, afterPath)).toThrow();
+  const unknownPath = './__tests__/__fixtures__/unknown.json';
+  expect(() => genDiff(unknownPath, afterPath)).toThrow();
 });
