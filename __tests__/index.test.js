5933c881c95e74f72581ae4287d79db20ae9a7a6
Mihail <freon2006@bk.ru>
2018-10-09 23:33:33 +0500

    test fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,15 +6,15 @@ const beforeJSONPath = './__tests__/__fixtures__/json/before.json';
 const afterJSONPath = './__tests__/__fixtures__/json/after.json';
 const emptyJSONPath = './__tests__/__fixtures__/json/empty.json';
 
-const result1 = './__tests__/__fixtures__/result1.txt';
-const result2 = './__tests__/__fixtures__/result2.txt';
+const result1 = fs.readFileSync('./__tests__/__fixtures__/result1.txt', 'utf8');
+const result2 = fs.readFileSync('./__tests__/__fixtures__/result2.txt', 'utf8');
 
 test('Test JSON file', () => {
-  expect(genDiff(beforeJSONPath, afterJSONPath)).toBe(fs.readFileSync(result1, 'utf8'));
+  expect(genDiff(beforeJSONPath, afterJSONPath)).toBe(result1);
 });
 
 test('Test JSON empty file', () => {
-  expect(genDiff(emptyJSONPath, afterJSONPath)).toBe(fs.readFileSync(result2, 'utf8'));
+  expect(genDiff(emptyJSONPath, afterJSONPath)).toBe(result2);
 });
 
 
@@ -23,11 +23,11 @@ const afterYAMLPath = './__tests__/__fixtures__/yaml/after.yaml';
 const emptyYAMLPath = './__tests__/__fixtures__/yaml/empty.yaml';
 
 test('Test YAML file', () => {
-  expect(genDiff(beforeYAMLPath, afterYAMLPath)).toBe(fs.readFileSync(result1, 'utf8'));
+  expect(genDiff(beforeYAMLPath, afterYAMLPath)).toBe(result1);
 });
 
 test('Test YAML empty file', () => {
-  expect(genDiff(emptyYAMLPath, afterYAMLPath)).toBe(fs.readFileSync(result2, 'utf8'));
+  expect(genDiff(emptyYAMLPath, afterYAMLPath)).toBe(result2);
 });
 
 // test('Test YAML error file', () => {
@@ -41,11 +41,11 @@ const afterINIPath = './__tests__/__fixtures__/ini/after.ini';
 const emptyINIPath = './__tests__/__fixtures__/ini/empty.ini';
 
 test('Test INI file', () => {
-  expect(genDiff(beforeINIPath, afterINIPath)).toBe(fs.readFileSync(result1, 'utf8'));
+  expect(genDiff(beforeINIPath, afterINIPath)).toBe(result1);
 });
 
 test('Test INI empty file', () => {
-  expect(genDiff(emptyINIPath, afterINIPath)).toBe(fs.readFileSync(result2, 'utf8'));
+  expect(genDiff(emptyINIPath, afterINIPath)).toBe(result2);
 });
 
 // test('Test INI error file', () => {
